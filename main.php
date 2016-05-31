738ec8677f03aa25e1ff0614a23cdfa973172990
Ratna Eluri <ratna@NAVOMIs-MacBook-Pro.local>
2016-05-31 14:57:28 -0400

    Changed main.php

--- main.php
+++ main.php
@@ -1,4 +1,22 @@
+<html>
+<head>
+<scipt>
+	function fullName(firstName, lastName, callback){
+	  console.log("My name is " + firstName + " " + lastName);
+	  callback(lastName);
+	}
+
+	var greeting = function(ln){
+	  console.log('Welcome Mr. ' + ln);
+	};
+
+	fullName("Jackie", "Chan", greeting);
+</script>
+</head>
+<body>
 <?php
-    echo 'Hello Ratna Kireeti Eluri! Welcome to ratna-test-101 PHP app. ';
+    echo 'Hello Ratna Kireeti Eluri! Welcome to ratna-test-101 PHP app with script. ';
     echo 'This is another test.'
 ?>    
+</body>
+</html>
