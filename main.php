817f7fc6ddecd06af927979d2b58f4aab41b376f
Ratna Eluri <ratna@NAVOMIs-MacBook-Pro.local>
2016-05-31 15:29:45 -0400

    Added PostgresQL DB Create and Select Operation.

--- main.php
+++ main.php
@@ -37,6 +37,62 @@
    } else {
       echo "Opened database successfully\n";
    }
+
+   $sql =<<<EOF
+      CREATE TABLE COMPANY
+      (ID INT PRIMARY KEY     NOT NULL,
+      NAME           TEXT    NOT NULL,
+      AGE            INT     NOT NULL,
+      ADDRESS        CHAR(50),
+      SALARY         REAL);
+EOF;
+
+   $ret = pg_query($db, $sql);
+   if(!$ret){
+      echo pg_last_error($db);
+   } else {
+      echo "Table created successfully\n";
+   }
+
+   $sql1 =<<<EOF
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (1, 'Paul', 32, 'California', 20000.00 );
+
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (2, 'Allen', 25, 'Texas', 15000.00 );
+
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );
+
+      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
+      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );
+EOF;
+
+   $ret1 = pg_query($db, $sql1);
+   if(!$ret1){
+      echo pg_last_error($db);
+   } else {
+      echo "Records created successfully\n";
+   }
+
+$sql2 =<<<EOF
+      SELECT * from COMPANY;
+EOF;
+
+   $ret2 = pg_query($db, $sql2);
+   if(!$ret2){
+      echo pg_last_error($db);
+      exit;
+   } 
+   while($row = pg_fetch_row($ret2)){
+      echo "ID = ". $row[0] . "\n";
+      echo "NAME = ". $row[1] ."\n";
+      echo "ADDRESS = ". $row[2] ."\n";
+      echo "SALARY =  ".$row[4] ."\n\n";
+   }
+   echo "Operation done successfully\n";
+
+   pg_close($db);
 ?>  
 </body>
 </html>
